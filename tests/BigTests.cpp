16603339eb63beb8cab58c632da6378a1287fc5c
Olivier LDff <olivier.ldff@gmail.com>
2021-04-07 11:12:06 +0200

    ✅ BigTests: SET_UINT8/16/32/48/64

--- tests/BigTests.cpp
+++ tests/BigTests.cpp
@@ -1,5 +1,6 @@
 ﻿#include <Endn/Big.hpp>
 #include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
 
 using namespace endn::big;
 
@@ -59,4 +60,39 @@ TEST(Big, GET_UINT64)
         std::uint8_t buffer[9] = {0x00, 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0};
         ASSERT_EQ(GET_UINT64(buffer, 1), 0x123456789ABCDEF0);
     }
+}
+
+TEST(Big, SET_UINT8)
+{
+    std::uint8_t bufferSet[1];
+    SET_INT8(bufferSet, 0x12);
+    ASSERT_THAT(bufferSet, testing::ElementsAre(0x12));
+}
+
+TEST(Big, SET_UINT16)
+{
+    std::uint8_t bufferSet[2];
+    SET_INT16(bufferSet, 0x1234);
+    ASSERT_THAT(bufferSet, testing::ElementsAre(0x12, 0x34));
+}
+
+TEST(Big, SET_UINT32)
+{
+    std::uint8_t bufferSet[4];
+    SET_INT32(bufferSet, 0x12345678);
+    ASSERT_THAT(bufferSet, testing::ElementsAre(0x12, 0x34, 0x56, 0x78));
+}
+
+TEST(Big, SET_UINT48)
+{
+    std::uint8_t bufferSet[6];
+    SET_INT48(bufferSet, 0x123456789ABC);
+    ASSERT_THAT(bufferSet, testing::ElementsAre(0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC));
+}
+
+TEST(Big, SET_UINT64)
+{
+    std::uint8_t bufferSet[8];
+    SET_INT64(bufferSet, 0x123456789ABCDEF0);
+    ASSERT_THAT(bufferSet, testing::ElementsAre(0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0));
 }
